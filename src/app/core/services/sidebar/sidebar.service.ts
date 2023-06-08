4e5a9af7a722cdfac20289162920aa7f95f4fac5
pedro <pedro.jcaceresl@gmail.com>
2023-06-07 21:10:23 -0400

    added click outside sidebar function

--- src/app/core/services/sidebar/sidebar.service.ts
+++ src/app/core/services/sidebar/sidebar.service.ts
@@ -5,14 +5,25 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class SidebarService {
+  private readonly STORAGE_KEY = 'sidebarCollapsed';
+
   $collapsed = new BehaviorSubject<boolean>(false);
 
   setCollapsed(collapsed: boolean) {
     this.$collapsed.next(collapsed);
+    localStorage.setItem(this.STORAGE_KEY, String(collapsed)); // Store the collapsed state in localStorage
   }
 
   toggleCollapsed() {
     const collapsed = this.$collapsed.getValue();
     this.setCollapsed(!collapsed);
+  }
+
+  loadCollapsedState() {
+    const collapsedState = localStorage.getItem(this.STORAGE_KEY);
+    const isMobile = window.innerWidth < 768;
+    const initialState =
+      collapsedState !== null ? collapsedState === 'true' : isMobile;
+    this.setCollapsed(initialState);
   }
 }
--- src/app/layout/pages/layout-page/layout-page.component.ts
+++ src/app/layout/pages/layout-page/layout-page.component.ts
@@ -8,12 +8,20 @@ import { SidebarService } from 'src/app/core/services';
 })
 export class LayoutPageComponent {
   components = components;
-
-  constructor(public sidebarService: SidebarService) {}
+  isMobile!: boolean;
+  constructor(public sidebarService: SidebarService) {
+    sidebarService.loadCollapsedState();
+  }
 
   @HostListener('window:resize', ['$event'])
   onWindowResize(event: any) {
-    const isMobile = window.innerWidth < 768; // Adjust the threshold as per your needs
-    this.sidebarService.setCollapsed(isMobile);
+    this.isMobile = window.innerWidth < 768; // Adjust the threshold as per your needs
+    this.sidebarService.setCollapsed(this.isMobile);
   }
+
+  onClickOutside() {
+    if (!this.isMobile) return;
+    this.sidebarService.setCollapsed(true);
+  }
+
 }
